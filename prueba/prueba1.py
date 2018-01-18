f014415c0459eb35ec5285155c3c66cfeacf7387
1-21-01-22B-02\pc1-21-01-22B-03 <Darwin19970420@hotmail.com>
2018-01-18 10:22:49 -0500

    Implementacion de los arcos y la codiicaicon del movimiento del blaòn

--- prueba/prueba1.py
+++ prueba/prueba1.py
@@ -3,19 +3,25 @@ tk = Tk()
 pos_inic = 800
 canvas = Canvas(tk, width=900, height=800)
 
-imagen = PhotoImage(file="arco.gif")
-canvas.create_image(50,100, anchor=NW, image=imagen)
+imagen = PhotoImage(file="A1.gif")
+canvas.create_image(650,100, anchor=NW, image=imagen)
+image1 = PhotoImage(file="ARCOIZQUIERDO.gif")
+canvas.create_image(50,100, anchor=NW, image=image1)
 balon = PhotoImage(file="ball.gif", width=500, height=500)
-canvas.create_image(800,350, anchor=NW, image=balon)
+canvas.create_image(450,250, anchor=NW, image=balon)
 
 
 def movimimiento(event):
-	global pos_inic
-	if event.keysym == 'Up':
-		canvas.move(2, -5, 0)
-		pos_inic = pos_inic - 5
-		print(pos_inic)
-
-canvas.bind_all('<KeyPress-Up>', movimimiento)
+        global pos_inic
+        if event.keysym == 'Left':
+                canvas.move(3, -5, 0)
+                pos_inic = pos_inic - 5
+                print(pos_inic)
+        elif event.keysym == 'Right':
+                canvas.move(3, 5, 0)
+                pos_inic = pos_inic + 5
+                print(pos_inic)
+canvas.bind_all('<KeyPress-Left>', movimimiento)
+canvas.bind_all('<KeyPress-Right>', movimimiento)
 canvas.pack()
 canvas.mainloop()
