17703058b85fa319b3b0e471db4a1d269de9edfd
Markus\Ihoaxcel <marco.vele@epn.edu.ec>
2018-01-18 19:08:43 -0500

    Verificación de gol en el arco derecho

--- prueba/prueba1.py
+++ prueba/prueba1.py
@@ -1,7 +1,8 @@
 from tkinter import *
 tk = Tk()
-pos_inic = 800
-gol = 12
+inBal_x = 450
+inBal_y = 250
+
 
 canvas = Canvas(tk, width=900, height=500)
 imagen = PhotoImage(file="A1.gif")
@@ -9,29 +10,82 @@ canvas.create_image(650,100, anchor=NW, image=imagen)
 image1 = PhotoImage(file="ARCOIZQUIERDO.gif")
 canvas.create_image(50,100, anchor=NW, image=image1)
 balon = PhotoImage(file="ball.gif", width=500, height=500)
-canvas.create_image(450,250, anchor=NW, image=balon)
+canvas.create_image(inBal_x,inBal_y, anchor=NW, image=balon)
+canvas.create_text(80, 10, text='Prueba Gol', fill='green')
+
+def movertriangle (event):
+    global inBal_x, inBal_y
+    if event.keysym == 'Up':
+        x = 0
+        y = -5
+        canvas.move(3,x,y)
+        inBal_y = inBal_y  + y
+
+        ## llamado funcion para saber si es gol (goliz)
+        goliz(inBal_x,inBal_y)
+        
+        print (inBal_y,inBal_x)
+        
+    elif event.keysym == 'Down':
+        x = 0
+        y = +5
+        canvas.move(3,x,y)
+        inBal_y = inBal_y +y
+
+        ## llamado funcion para saber si es gol (goliz)
+        goliz(inBal_x,inBal_y)
+        
+        print (inBal_y,inBal_x)
+         
+    elif event.keysym == 'Left':
+        x = -5
+        y = 0
+        canvas.move(3,x, y)
+        inBal_x = inBal_x + x
+
+        ## llamado funcion para saber si es gol (goliz)
+        goliz(inBal_x,inBal_y)
+
+        print (inBal_y,inBal_x)
+        
+    elif event.keysym == 'Right':
+        x =+5
+        y= 0
+        canvas.move(3,x, y)
+        inBal_x = inBal_x + x
+
+        ## llamado funcion para saber si es gol (goliz)
+        goliz(inBal_x,inBal_y)
+        
+        print (inBal_y,inBal_x)
+
+def goliz(x,y):
+    ## Verificación del gol
+    if (x == 195  & y == 195):
+            print ("Gol")
+            mensaje()           
+    elif x== 705:
+        if y == 185:
+            mensaje()
+    else:
+            print (" ")                
+
+def mensaje():
+    ventana_msg = Tk()
+    ventana_msg.title("Aviso")
+    ventana_msg.geometry("100x100+350+250")
+    msg = Message(ventana_msg, text="Gol", relief=RIDGE, bd=1)
+    msg.pack(expand=YES, fill=BOTH)
+    
+    
 
 
-def movimimiento(event):
-        global pos_inic
-        if event.keysym == 'Left':
-                canvas.move(3, -5, 0)
-                pos_inic = pos_inic - 5
-                print(pos_inic)
-                if pos_inic == 475 or  pos_inic == 585:
-                        ventana_msg = Tk()
-                        ventana_msg.title("Aviso")
-                        ventana_msg.geometry("100x100+350+250")
-                        msg = Message(ventana_msg, text="Gol", relief=RIDGE, bd=5)
-                        msg.pack(expand=YES, fill=BOTH)
-                else:
-                        print()
-        elif event.keysym == 'Right':
-                canvas.move(3, 5, 0)
-                pos_inic = pos_inic + 5
-                print(pos_inic)
+canvas.bind_all('<KeyPress-Up>',movertriangle)
+canvas.bind_all('<KeyPress-Down>',movertriangle)
+canvas.bind_all('<KeyPress-Left>',movertriangle)
+canvas.bind_all('<KeyPress-Right>',movertriangle)
 
-canvas.bind_all('<KeyPress-Left>', movimimiento)
-canvas.bind_all('<KeyPress-Right>', movimimiento)
+
 canvas.pack()
 canvas.mainloop()
+
